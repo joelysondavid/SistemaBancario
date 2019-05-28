93302214006209b4867d9ca5b670b11e3c1e5492
Alisson <alissondreis@hotmail.com>
2019-05-27 22:18:20 -0300

    eu
    eu

---
+++ Sistema/src/model/ContaDAO.java
@@ -0,0 +1,33 @@
+
+package model;
+
+import framework.DaoConexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alisson R
+ */
+public class ContaDAO {
+    // varoaveos de para integração do banco com a classe Clientes
+    private Connection conn = null;
+    private PreparedStatement stm;
+    private ResultSet rs;
+    private DaoConexao dao;
+
+    // consturtor com a conexão vindo do framework/DaoConexao
+    public ContaDAO() {
+        // instancia uma nova conexao
+        dao = new DaoConexao();
+        try {
+            // nosso Connnection recebe a conexão
+            conn = dao.getConexao();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao tentar conexão com banco de dados: " + ex, "Erro de Conexão!", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
